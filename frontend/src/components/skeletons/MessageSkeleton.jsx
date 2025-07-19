d20792a1c950543bbc8644f291b05c57b98a66da
Vishwasai_Cheedalla <124761365+vishwasaicheedalla@users.noreply.github.com>
2025-07-19 20:02:24 +0530

    Update MessageSkeleton.jsx

--- frontend/src/components/skeletons/MessageSkeleton.jsx
+++ frontend/src/components/skeletons/MessageSkeleton.jsx
@@ -1,6 +1,6 @@
 const MessageSkeleton = () => {
   // Create an array of 6 items for skeleton messages
-  const skeletonMessages = Array(6).fill(null);
+  const skeletonMessages = Array(4).fill(null);
 
   return (
     <div className="flex-1 overflow-y-auto p-4 space-y-4">
